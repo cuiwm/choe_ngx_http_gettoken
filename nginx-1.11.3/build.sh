78e9b42066570d49c4ed8159c9a11dc236848179
cuiwm <xcuiwm@hotmail.com>
2016-12-19 16:01:46 +0800

    add build script

---
+++ nginx-1.11.3/build.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: build.sh
+# Author: xgod
+# mail: xgod@163.com
+# Created Time: Mon 19 Dec 2016 03:57:06 PM CST
+#########################################################################
+#!/bin/bash
+
+./configure --with-debug --prefix=/data/nginx 
